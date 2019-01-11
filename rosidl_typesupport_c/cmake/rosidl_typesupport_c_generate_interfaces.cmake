003c881371b449a16f7d42323e3ef7f3ef0b34dc
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-01-11 15:00:44 -0800

    Rebase from 'upstream'

--- rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
+++ rosidl_typesupport_c/cmake/rosidl_typesupport_c_generate_interfaces.cmake
@@ -138,11 +138,12 @@ if(NOT typesupports MATCHES ";")
     ${rosidl_generate_interfaces_TARGET}__${typesupports})
 else()
   if("${rosidl_typesupport_c_LIBRARY_TYPE}" STREQUAL "STATIC")
-    message(FATAL_ERROR "Multiple typesupports but static linking was requested")
+    message(FATAL_ERROR "Multiple typesupports [${typesupports}] but static "
+      "linking was requested")
   endif()
   if(NOT rosidl_typesupport_c_SUPPORTS_POCO)
-    message(FATAL_ERROR "Multiple typesupports but Poco was not available when "
-      "rosidl_typesupport_c was built")
+    message(FATAL_ERROR "Multiple typesupports [${typesupports}] but Poco was "
+      "not available when rosidl_typesupport_c was built")
   endif()
 endif()
 
@@ -191,7 +192,8 @@ if(BUILD_TESTING AND rosidl_generate_interfaces_ADD_LINTER_TESTS)
     ament_uncrustify(
       TESTNAME "uncrustify_rosidl_typesupport_c"
       # the generated code might contain longer lines for templated types
-      MAX_LINE_LENGTH 999
+      # a value of zero tells uncrustify to ignore line lengths
+      MAX_LINE_LENGTH 0
       "${_output_path}")
   endif()
 endif()
